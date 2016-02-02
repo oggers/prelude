3a8eb7d7a2cc269a66523eed308ff35c579db332
Juan Carlos Coruña <oggers@gmail.com>
2016-02-02 15:35:40 +0100

    Fix description

--- personal/comint.el
+++ personal/comint.el
@@ -4,7 +4,7 @@
 ;;; Code:
 (defvar comint-password-prompt-regexp)
 (setq comint-password-prompt-regexp
-      ;; Add [SUDO] in order to hide password prompt for docker
+      ;; Add [SUDO] in order to hide password prompt for ansible-playbook --ask-become-pass
       "\\(^ *\\|\\( SMB\\|'s\\|Bad\\|CVS\\|Enter\\(?: \\(?:\\(?:sam\\|th\\)e\\)\\)?\\|Kerberos\\|LDAP\\|New\\|Old\\|Repeat\\|UNIX\\|\\[sudo]\\|[SUDO]\\|enter\\(?: \\(?:\\(?:sam\\|th\\)e\\)\\)?\\|login\\|new\\|old\\) +\\)\\(?:Pass\\(?: phrase\\|phrase\\|word\\)\\|Response\\|pass\\(?: phrase\\|phrase\\|word\\)\\)\\(?:\\(?:, try\\)? *again\\| (empty for no passphrase)\\| (again)\\)?\\(?: for [^:]+\\)?:\\s *\\'"
       )
 
