414e5b1084c917d9deee3ac8d963cf7e5db39598
Juan Carlos Coruña <oggers@gmail.com>
2016-02-02 14:09:13 +0100

    Add some shell enhancements

--- personal/comint.el
+++ personal/comint.el
@@ -7,5 +7,23 @@
       ;; Add [SUDO] in order to hide password prompt for docker
       "\\(^ *\\|\\( SMB\\|'s\\|Bad\\|CVS\\|Enter\\(?: \\(?:\\(?:sam\\|th\\)e\\)\\)?\\|Kerberos\\|LDAP\\|New\\|Old\\|Repeat\\|UNIX\\|\\[sudo]\\|[SUDO]\\|enter\\(?: \\(?:\\(?:sam\\|th\\)e\\)\\)?\\|login\\|new\\|old\\) +\\)\\(?:Pass\\(?: phrase\\|phrase\\|word\\)\\|Response\\|pass\\(?: phrase\\|phrase\\|word\\)\\)\\(?:\\(?:, try\\)? *again\\| (empty for no passphrase)\\| (again)\\)?\\(?: for [^:]+\\)?:\\s *\\'"
       )
+
+(custom-set-variables
+ '(comint-scroll-to-bottom-on-input t)  ; always insert at the bottom
+ '(comint-scroll-to-bottom-on-output t) ; always add output at the bottom
+ '(comint-scroll-show-maximum-output t) ; scroll to show max possible output
+ '(comint-completion-autolist t)        ; show completion list when ambiguous
+ '(comint-input-ignoredups t)           ; no duplicates in command history
+ '(comint-completion-addsuffix t)       ; insert space/slash after file completion
+)
+
+; interpret and use ansi color codes in shell output windows
+(ansi-color-for-comint-mode-on)
+
+; make completion buffers disappear after 3 seconds.
+(add-hook 'completion-setup-hook
+          (lambda () (run-at-time 3 nil
+                                  (lambda () (delete-windows-on "*Completions*")))))
+
 (provide 'custom)
 ;;; comint.el ends here
